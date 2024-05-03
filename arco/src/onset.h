0da23fd6db48ce80eaf37f5501e2fb246019d18a
fitboye <49235923+fitboye@users.noreply.github.com>
2024-05-02 21:12:03 -0400

    Onset detector now evaluates each input channel separately and sends a message indicating the channel.
    Freeing memory has also been fixed.

--- arco/src/onset.h
+++ arco/src/onset.h
@@ -8,7 +8,6 @@
 
 extern const char *Onset_name;
 
-
 class Onset : public Ugen {
   public:
     const char *address; // where to send messages
@@ -17,12 +16,19 @@ class Onset : public Ugen {
     int input_stride;
     Sample_ptr input_samps;
     
-    int samps_stored;
     int frame_size;
     int buffer_size;
-    double *frame;
-    LPSpectralDifferenceODF* odf;
-    RTOnsetDetection* detector;
+    int input_chans;
+    int samps_stored;
+    
+    // one for each channel
+    Vec<double*> frames;
+    LPSpectralDifferenceODF** odfs;
+    RTOnsetDetection** detectors;
+    
+    // for freeing
+    static Vec<LPSpectralDifferenceODF*> old_odfs;
+    static Vec<RTOnsetDetection*> old_detectors;
     
     Onset(int id, Ugen_ptr input, const char *address_) : Ugen(id, 0, 0) {
         // round up to multiple of BL:
@@ -32,19 +38,33 @@ class Onset : public Ugen {
         samps_stored = 0;
         frame_size = 2048;
         buffer_size = 512;
-        frame = O2_MALLOCNT(frame_size, double);
-        odf = new LPSpectralDifferenceODF(); //Vec
-        odf->init();
-        detector = new RTOnsetDetection();
+        input_chans = input->chans;
+        
+        odfs = O2_MALLOCNT(input_chans, LPSpectralDifferenceODF*);
+        detectors = O2_MALLOCNT(input_chans, RTOnsetDetection*);
+        frames.init(input_chans, false, true);
+        for (int i = 0; i < input_chans; i++){
+            if (old_odfs.size() > 0) odfs[i] = old_odfs.pop_back();
+            else odfs[i] = new LPSpectralDifferenceODF();
+            odfs[i]->init();
+            
+            detectors[i] = new RTOnsetDetection();
+            
+            frames[i] = O2_MALLOCNT(frame_size, double);
+        }
     }
 
 
     ~Onset() {
         O2_FREE((char *) address);
-        O2_FREE(frame);
-        //NEED TO CHANGE THIS
-//        delete odf;
-//        delete detector;
+        for (int i = 0; i < input_chans; i++){
+            old_odfs.push_back(odfs[i]);
+            old_detectors.push_back(detectors[i]);
+            O2_FREE(frames[i]);
+        }
+        frames.finish();
+        O2_FREE(odfs);
+        O2_FREE(detectors);
     }
 
 
@@ -55,30 +75,35 @@ class Onset : public Ugen {
     void real_run() {
         input_samps = input->run(current_block);
         
-        if (samps_stored == frame_size){ //if we have full frame
-            for (int i = 0; i < frame_size - buffer_size; i++){ // i < buffer size
-                frame[i] = frame[i + buffer_size];
+        if (samps_stored == frame_size){ // we have a full frame, delete first frame and shift rest
+            for (int i = 0; i < input_chans; i++){
+                for (int j = 0; j < frame_size - buffer_size; j++){
+                    frames[i][j] = frames[i][j + buffer_size];
+                }
             }
-            samps_stored = 0;
         }
         
-        //currently only uses one channel
-        for (int i = 0; i < BL; i++){ //copy one block to frame buffer
-            frame[i + samps_stored] = *(&input_samps[i]);
+        for (int i = 0; i < input_chans; i++){ //copy one block to each frame buffer
+            for (int j = 0; j < BL; j++){
+                frames[i][j + samps_stored] = *(&input_samps[i*BL + j]);
+            }
         }
+        
         samps_stored += BL;
-        
-        int n = 10;
-        
         if (samps_stored != frame_size) return; //compute odf if we have full frame
         
-        float odf_res = odf->process_frame(frame_size, frame);
-        
-        if (detector->is_onset(odf_res)){
-            o2sm_send_start();
-            o2sm_add_int32(id);
-            o2sm_add_float(odf_res);
-            o2sm_send_finish(0, address, false);
+        for (int i = 0; i < input_chans; i++){
+            float odf_res = odfs[i]->process_frame(frame_size, frames[i]);
+            if (detectors[i]->is_onset(odf_res)){
+                o2sm_send_start();
+                o2sm_add_int32(id);
+                o2sm_add_int32(i); // send which channel
+                o2sm_send_finish(0, address, false);
+            }
         }
+        samps_stored = 0;
     }
 };
+
+Vec<LPSpectralDifferenceODF*> Onset::old_odfs;
+Vec<RTOnsetDetection*> Onset::old_detectors;
